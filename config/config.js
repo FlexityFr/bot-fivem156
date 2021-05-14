824c69ef845ce19a3eb49f31d43368cc2910ac97
FlexityFr <81111489+FlexityFr@users.noreply.github.com>
2021-05-14 17:11:16 +0200

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports.options = {
+    token: 'ODQyNzgwMzMyNzI5ODI3NDI4.YJ6SWQ.NfFTs0pfzHcHNO7q9-w9MJuFqus',
+    topicChannels: [
+      'general'
+    ],
+    roles: ["Admin"],
+    servers: [
+        { name: 'Shatain City Rp', url: 'http://127.0.0.1:30120', alias: 'Shatain City Rp' },
+    ],
+    pollRate: 30,
+};
